14d1d296884f97dd890e7b6f5e0fbbe37b9ab15a
Thomas Schoffelen <thomas@schof.co>
2025-08-22 18:01:59 +0100

    feat: export error

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export { defineTable } from "./table";
 export { defineEntity } from "./entity";
 
+export { TurbineError } from "./error";
+
 export * from "./types/key";
 export * from "./types/table";
 export * from "./types/entity";
